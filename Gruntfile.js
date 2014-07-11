db1a4fd67339310737aba132375c6c586ab995c5
刘哲 <sking.pt@gmail.com>
2014-07-11 15:34:06 +0800

    f

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,53 @@
-reload/Gruntfile.js
+module.exports = function(grunt) {
+  grunt.registerTask('watch', [ 'watch' ]);
+  grunt.initConfig({
+    concat: {
+      js: {
+        options: {
+          separator: ';'
+        },
+        src: [
+          'doc/index.js',
+          'static/ventor/*.js',
+          'static/js/*.js'
+        ],
+        dest: 'static/dest/main.min.js'
+      },
+    },
+    uglify: {
+      options: {
+        mangle: false
+      },
+      js: {
+        files: {
+          'static/dest/main.min.js': ['static/dest/main.min.js'],
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: ['static/js/*.js', 'doc/index.js'],
+        tasks: ['concat:js', 'uglify:js'],
+        options: {
+          livereload: true,
+        }
+      },
+	  html:{
+        files: ['index.html'],
+        options: {
+          livereload: true,
+        }
+	  },
+      css: {
+        files: ['static/css/*.css'],
+        options: {
+          livereload: true,
+        }
+      }
+    }
+  });
+ 
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+};
