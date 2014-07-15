dff8cdcbe7caee6f43ba4eba1eb5c144d032efd4
刘哲 <sking.pt@gmail.com>
2014-07-15 13:05:24 +0800

    update img

--- doc/index.js
+++ doc/index.js
@@ -1,183 +1,209 @@
 window.indexData = {
     "1": [
         {
-            "name": "draggable",
-            "url": "../avalon.oniui/draggable/avalon.draggable.doc.html",
-            "title": "拖动组件",
-            "des": "暂无描述",
-            "cover": "doc/img/draggable.png"
-        },
-        {
-            "name": "position",
-            "url": "../avalon.oniui/position/avalon.position.doc.html",
-            "title": "position2组件",
-            "des": "暂无描述",
-            "cover": "doc/img/position.png"
-        }
-    ],
-    "2": [
-        {
-            "name": "accordion",
-            "url": "../avalon.oniui/accordion/avalon.accordion.doc.html",
-            "title": "accordion文档说明",
-            "des": "暂无描述",
-            "cover": "doc/img/accordion.png"
-        },
-        {
-            "name": "dropdown",
-            "url": "../avalon.oniui/dropdown/avalon.dropdown.doc.html",
-            "title": "dropdown",
-            "des": "dropdown的描述",
-            "cover": "doc/img/dropdown.png"
-        },
-        {
-            "name": "resizable",
-            "url": "../avalon.oniui/resizable/avalon.resizable.doc.html",
-            "title": "resizable",
-            "des": "暂无描述",
-            "cover": "doc/img/resizable.png"
-        },
-        {
-            "name": "switchdropdown",
-            "url": "../avalon.oniui/switchdropdown/avalon.switchdropdown.doc.html",
-            "title": "dropdown",
-            "des": "暂无描述",
-            "cover": "doc/img/switchdropdown.png"
-        }
-    ],
-    "3": [
-        {
-            "name": "checkboxlist",
-            "url": "../avalon.oniui/checkboxlist/avalon.checkboxlist.doc.html",
-            "title": "checkboxlist文档说明",
-            "des": "暂无描述",
-            "cover": "doc/img/checkboxlist.png"
-        },
-        {
-            "name": "switch",
-            "url": "../avalon.oniui/switch/avalon.switch.doc.html",
-            "title": "switch组件",
-            "des": "暂无描述",
-            "cover": "doc/img/switch.png"
-        }
-    ],
-    "4": [
-        {
-            "name": "slider",
-            "url": "../avalon.oniui/slider/avalon.slider.doc.html",
-            "title": "slider文档说明",
-            "des": "暂无描述",
-            "cover": "doc/img/slider.png"
-        }
-    ],
-    "5": [
-        {
-            "name": "at",
-            "url": "../avalon.oniui/at/avalon.at.doc.html",
-            "title": "avalon类似新浪微博的@提示插件",
-            "des": "暂无描述",
-            "cover": "doc/img/at.png"
-        },
-        {
-            "name": "miniswitch",
-            "url": "../avalon.oniui/miniswitch/avalon.miniswitch.doc.html",
-            "title": "dropdown",
-            "des": "暂无描述",
-            "cover": "doc/img/miniswitch.png"
-        },
-        {
-            "name": "pager",
-            "url": "../avalon.oniui/pager/avalon.pager.doc.html",
-            "title": "分页组件",
-            "des": "暂无描述",
-            "cover": "doc/img/pager.png"
-        },
-        {
-            "name": "textbox",
-            "url": "../avalon.oniui/textbox/avalon.textbox.doc.html",
-            "title": "textbox文档说明",
-            "des": "暂无描述",
-            "cover": "doc/img/textbox.png"
-        }
-    ],
-    "6": [
-        {
-            "name": "scrollbar",
-            "url": "../avalon.oniui/scrollbar/avalon.scrollbar.doc.html",
-            "title": "scrollbar组件",
-            "des": "暂无描述",
-            "cover": "doc/img/scrollbar.png"
-        }
-    ],
-    "7": [
-        {
-            "name": "notice",
-            "url": "../avalon.oniui/notice/avalon.notice.doc.html",
-            "title": "notice文档说明",
-            "des": "暂无描述",
-            "cover": "doc/img/notice.png"
-        }
-    ],
-    "8": [
-        {
-            "name": "progressbar",
-            "url": "../avalon.oniui/progressbar/avalon.progressbar.doc.html",
-            "title": "progressbar组件",
-            "des": "暂无描述",
-            "cover": "doc/img/progressbar.png"
-        }
-    ],
-    "9": [
-        {
-            "name": "simplegrid",
-            "url": "../avalon.oniui/simplegrid/avalon.simplegrid.doc.html",
-            "title": "grid文档",
-            "des": "暂无描述",
-            "cover": "doc/img/simplegrid.png"
-        },
-        {
-            "name": "spinner",
-            "url": "../avalon.oniui/spinner/avalon.spinner.doc.html",
-            "title": "spinner文档说明",
-            "des": "暂无描述",
-            "cover": "doc/img/spinner.png"
-        },
-        {
-            "name": "tooltip",
-            "url": "../avalon.oniui/tooltip/avalon.tooltip.doc.html",
-            "title": "tooltip组件",
-            "des": "暂无描述",
-            "cover": "doc/img/tooltip.png"
-        }
-    ],
-    "10": [
-        {
-            "name": "datepicker",
-            "url": "../avalon.oniui/datepicker/avalon.datepicker.doc.html",
-            "title": "notice文档说明",
-            "des": "暂无描述",
-            "cover": "doc/img/datepicker.png"
-        },
-        {
             "name": "dialog",
-            "url": "../avalon.oniui/dialog/avalon.dialog.doc.html",
+            "url": "src/avalon.oniui/dialog/avalon.dialog.doc.html",
             "title": "dialog文档说明",
             "des": "暂无描述",
             "cover": "doc/img/dialog.png"
         },
         {
+            "name": "preview",
+            "url": "src/avalon.oniui/preview/avalon.preview.doc.html",
+            "title": "图片预览组件",
+            "des": "暂无描述",
+            "cover": "doc/img/preview.png"
+        },
+        {
+            "name": "store",
+            "url": "src/avalon.oniui/store/avalon.store.doc.html",
+            "title": "分页组件",
+            "des": "暂无描述",
+            "cover": "doc/img/store.png"
+        }
+    ],
+    "2": [
+        {
+            "name": "tooltip",
+            "url": "src/avalon.oniui/tooltip/avalon.tooltip.doc.html",
+            "title": "tooltip组件",
+            "des": "tooltip组件，给一个元素或者给元素里满足配置条件的系列元素添加一个富UI及交互的气泡提示框",
+            "cover": "doc/img/tooltip.png"
+        }
+    ],
+    "3": [
+        {
+            "name": "miniswitch",
+            "url": "src/avalon.oniui/miniswitch/avalon.miniswitch.doc.html",
+            "title": "miniswitch文档说明",
+            "des": "miniswitch基于switchdropdown实现图标式下拉开关。",
+            "cover": "doc/img/miniswitch.png"
+        },
+        {
+            "name": "tab",
+            "url": "src/avalon.oniui/tab/avalon.tab.doc.html",
+            "title": "tab组件",
+            "des": "tab组件，实现扫描DOM结构或者接受数组传参，生成tab，支持click、mouseenter事件响应切换，支持mouseenter情形延迟响应切换，支持click情形tab选中情况下再次点击回调，支持自动切换效果，支持tab增删禁用启用并可混合设置同步tab可删除状态，支持混合配制panel内容类型并支持panel内容是ajax配置回调",
+            "cover": "doc/img/tab.png"
+        },
+        {
+            "name": "textbox",
+            "url": "src/avalon.oniui/textbox/avalon.textbox.doc.html",
+            "title": "textbox文档说明",
+            "des": "通过给输入域input、textarea设置不同的配置项可以使表单拥有舒服的视觉效果，也可以使其具有提示补全功能",
+            "cover": "doc/img/textbox.png"
+        }
+    ],
+    "4": [
+        {
+            "name": "at",
+            "url": "src/avalon.oniui/at/avalon.at.doc.html",
+            "title": "avalon类似新浪微博的@提示插件",
+            "des": "暂无描述",
+            "cover": "doc/img/at.png"
+        },
+        {
+            "name": "dropdown",
+            "url": "src/avalon.oniui/dropdown/avalon.dropdown.doc.html",
+            "title": "dropdown文档说明",
+            "des": "dropdown基于avalon对普通select控件进行扩展，支持通过data初始化下拉选项，使用内置的scroll控件。",
+            "cover": "doc/img/dropdown.png"
+        },
+        {
+            "name": "notice",
+            "url": "src/avalon.oniui/notice/avalon.notice.doc.html",
+            "title": "notice文档说明",
+            "des": "暂无描述",
+            "cover": "doc/img/notice.png"
+        },
+        {
+            "name": "position",
+            "url": "src/avalon.oniui/position/avalon.position.doc.html",
+            "title": "position2组件",
+            "des": "暂无描述",
+            "cover": "doc/img/position.png"
+        },
+        {
+            "name": "resizable",
+            "url": "src/avalon.oniui/resizable/avalon.resizable.doc.html",
+            "title": "resizable",
+            "des": "暂无描述",
+            "cover": "doc/img/resizable.png"
+        }
+    ],
+    "5": [
+        {
+            "name": "flipswitch",
+            "url": "src/avalon.oniui/flipswitch/avalon.flipswitch.doc.html",
+            "title": "flipswitch组件",
+            "des": "flipswitch组件，将checkbox表单元素转化成富UI的开关",
+            "cover": "doc/img/flipswitch.png"
+        },
+        {
             "name": "hotkeys",
-            "url": "../avalon.oniui/hotkeys/avalon.hotkeys.doc.html",
+            "url": "src/avalon.oniui/hotkeys/avalon.hotkeys.doc.html",
             "title": "热键绑定",
             "des": "暂无描述",
             "cover": "doc/img/hotkeys.png"
         },
         {
-            "name": "tab",
-            "url": "../avalon.oniui/tab/avalon.tab.doc.html",
-            "title": "Tab组件",
+            "name": "scrollbar",
+            "url": "src/avalon.oniui/scrollbar/avalon.scrollbar.doc.html",
+            "title": "scrollbar组件",
+            "des": "scrollbar组件，自定义滚动条样式",
+            "cover": "doc/img/scrollbar.png"
+        },
+        {
+            "name": "spinner",
+            "url": "src/avalon.oniui/spinner/avalon.spinner.doc.html",
+            "title": "spinner文档说明",
             "des": "暂无描述",
-            "cover": "doc/img/tab.png"
+            "cover": "doc/img/spinner.png"
+        }
+    ],
+    "6": [
+        {
+            "name": "datepicker",
+            "url": "src/avalon.oniui/datepicker/avalon.datepicker.doc.html",
+            "title": "datepicker文档说明",
+            "des": "暂无描述",
+            "cover": "doc/img/datepicker.png"
+        },
+        {
+            "name": "pager",
+            "url": "src/avalon.oniui/pager/avalon.pager.doc.html",
+            "title": "分页组件",
+            "des": "暂无描述",
+            "cover": "doc/img/pager.png"
+        }
+    ],
+    "7": [
+        {
+            "name": "accordion",
+            "url": "src/avalon.oniui/accordion/avalon.accordion.doc.html",
+            "title": "accordion文档说明",
+            "des": "accordion组件是在有限的区域显示可折叠内容面板的信息，通过不同的配置选项和丰富的api可以灵活的设置accordion,实现比如嵌套accordion的效果",
+            "cover": "doc/img/accordion.png"
+        },
+        {
+            "name": "progressbar",
+            "url": "src/avalon.oniui/progressbar/avalon.progressbar.doc.html",
+            "title": "progressbar组件",
+            "des": "progressbar组件，可以通过接口控制或者随机模拟进度条效果，支持条形，圆形，倒计时等效果功能",
+            "cover": "doc/img/progressbar.png"
+        },
+        {
+            "name": "scrollspy",
+            "url": "src/avalon.oniui/scrollspy/avalon.scrollspy.doc.html",
+            "title": "scrollspy组件",
+            "des": "scrollspy组件，可通过监听元素原生的scroll事件或者结合scrollbar组件使用时，监听其scroll，根据scrollTop，scrollLeft计算判定配置指定的panel list应该切换到什么位置",
+            "cover": "doc/img/scrollspy.png"
+        },
+        {
+            "name": "switchdropdown",
+            "url": "src/avalon.oniui/switchdropdown/avalon.switchdropdown.doc.html",
+            "title": "swtichdropdown文档说明",
+            "des": "switchdropdown基于dropdown实现下拉式开关。",
+            "cover": "doc/img/switchdropdown.png"
+        }
+    ],
+    "9": [
+        {
+            "name": "draggable",
+            "url": "src/avalon.oniui/draggable/avalon.draggable.doc.html",
+            "title": "拖动组件",
+            "des": "暂无描述",
+            "cover": "doc/img/draggable.png"
+        },
+        {
+            "name": "slider",
+            "url": "src/avalon.oniui/slider/avalon.slider.doc.html",
+            "title": "slider文档说明",
+            "des": "暂无描述",
+            "cover": "doc/img/slider.png"
+        }
+    ],
+    "10": [
+        {
+            "name": "checkboxlist",
+            "url": "src/avalon.oniui/checkboxlist/avalon.checkboxlist.doc.html",
+            "title": "checkboxlist文档说明",
+            "des": "暂无描述",
+            "cover": "doc/img/checkboxlist.png"
+        },
+        {
+            "name": "roleselect",
+            "url": "src/avalon.oniui/roleselect/avalon.roleselect.doc.html",
+            "title": "roleselect组件",
+            "des": "roleselect组件，以左右列表形式展示实现的复选组件",
+            "cover": "doc/img/roleselect.png"
+        },
+        {
+            "name": "simplegrid",
+            "url": "src/avalon.oniui/simplegrid/avalon.simplegrid.doc.html",
+            "title": "grid文档",
+            "des": "暂无描述",
+            "cover": "doc/img/simplegrid.png"
         }
     ]
 }
